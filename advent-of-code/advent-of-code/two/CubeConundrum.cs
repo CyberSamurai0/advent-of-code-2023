18fa441feed3d41f2ecf011b04fb62722876e30c
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2023-12-14 17:20:22 -0500

    Add day two

---
+++ advent-of-code/advent-of-code/two/CubeConundrum.cs
@@ -0,0 +1,10 @@
+namespace advent_of_code.two; 
+
+public class CubeConundrum {
+	private static readonly FileInput F = new ("./two/input.txt");
+
+	public static void Run() {
+		Console.WriteLine("Advent of Code 2023 Day 2: Cube Conundrum");
+		
+	}
+}
