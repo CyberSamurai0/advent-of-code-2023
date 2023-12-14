c33098eb69030a3a36b499c6ca52dbf97120e66d
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2023-12-14 17:15:50 -0500

    Finish Day 1 Part 2

--- advent-of-code/advent-of-code/one/Trebuchet.cs
+++ advent-of-code/advent-of-code/one/Trebuchet.cs
@@ -4,8 +4,52 @@ public class Trebuchet {
     private static readonly FileInput F = new ("./one/input.txt");
 
     public static void Run() {
+        string[] numberStrings = new [] {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        Dictionary<string, char> numberLookup = new Dictionary<string, char>();
+        numberLookup.Add("zero", '0');
+        numberLookup.Add("one", '1');
+        numberLookup.Add("two", '2');
+        numberLookup.Add("three", '3');
+        numberLookup.Add("four", '4');
+        numberLookup.Add("five", '5');
+        numberLookup.Add("six", '6');
+        numberLookup.Add("seven", '7');
+        numberLookup.Add("eight", '8');
+        numberLookup.Add("nine", '9');
+        
+        var sum = 0;
+        var sumWithStrings = 0;
+        List<int> calibrators = new List<int>();
+        List<int> calibratorsWithStrings = new List<int>();
+        Console.WriteLine($"{F.Lines.Length} loaded");
         foreach (var line in F.Lines) {
-            
+            List<char> numbers = new List<char>();
+            List<char> numbersWithStrings = new List<char>();
+            for (var i = 0; i < line.Length; i++) {
+                var c = line[i];
+                if (char.IsDigit(c)) {
+                    numbers.Add(c); 
+                    numbersWithStrings.Add(c);
+                }
+                if (char.IsLetter(c)) {
+                    foreach (var n in numberStrings) {
+                        if (i+n.Length <= line.Length && line.Substring(i, n.Length).Equals(n)) {
+                            numbersWithStrings.Add(numberLookup[n]);
+                            break;
+                        }
+                    }
+                }
+            }
+            if (numbers.Count >= 1) calibrators.Add(int.Parse(new string(new [] {numbers[0], numbers[^1]})));
+            if (numbersWithStrings.Count >= 1) calibratorsWithStrings.Add(int.Parse(new string(new [] {numbersWithStrings[0], numbersWithStrings[^1]})));
+            // Console.WriteLine($"{line}: {(numbers.Count >= 1 ? new string(new[] { numbers[0], numbers[^1] }) : "")}");
+            // Console.WriteLine($"{line}: {(numbersWithStrings.Count >= 1 ? new string(new[] { numbersWithStrings[0], numbersWithStrings[^1] }) : "")}");
         }
+
+        foreach (var c in calibrators) sum += c;
+        foreach (var c in calibratorsWithStrings) sumWithStrings += c;
+        
+        Console.WriteLine($"Sum (pt. 1): {sum}");
+        Console.WriteLine($"Sum (pt. 2): {sumWithStrings}");
     }
 }
