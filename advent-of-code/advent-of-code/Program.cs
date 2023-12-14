d5af69c5330dd64c368a7cecf4a0b36e8733e986
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2023-12-14 17:20:13 -0500

    Add day two support

--- advent-of-code/advent-of-code/Program.cs
+++ advent-of-code/advent-of-code/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using advent_of_code.one;
+using advent_of_code.two;
 
 string day = args.Length >= 1 ? args[0] : "1";
 
 if (day.Equals("1")) Trebuchet.Run();
+else if (day.Equals("2")) CubeConundrum.Run();
