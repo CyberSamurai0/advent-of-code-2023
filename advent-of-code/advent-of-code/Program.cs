44908773390213580db66ee4b54878c5a043e013
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2023-12-14 15:39:34 -0500

    Add code

--- advent-of-code/advent-of-code/Program.cs
+++ advent-of-code/advent-of-code/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using advent_of_code.one;
+
+string day = args.Length >= 1 ? args[0] : "1";
+
+if (day.Equals("1")) Trebuchet.Run();
--- advent-of-code/advent-of-code/one/Trebuchet.cs
+++ advent-of-code/advent-of-code/one/Trebuchet.cs
@@ -1,6 +1,11 @@
 namespace advent_of_code.one;
 
-public class Trebuchet
-{
-    
+public class Trebuchet {
+    private static readonly FileInput F = new ("./one/input.txt");
+
+    public static void Run() {
+        foreach (var line in F.Lines) {
+            
+        }
+    }
 }
