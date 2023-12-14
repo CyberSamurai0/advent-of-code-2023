569baa3b6454ed9e46eac2c353dab2a2516052a5
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2023-12-14 18:46:04 -0500

    Setup counting

--- advent-of-code/advent-of-code/two/CubeConundrum.cs
+++ advent-of-code/advent-of-code/two/CubeConundrum.cs
@@ -5,6 +5,25 @@ public static class CubeConundrum {
 
 	public static void Run() {
 		Console.WriteLine("Advent of Code 2023 Day 2: Cube Conundrum");
-		
+		Console.WriteLine($"{F.Lines.Length} loaded");
+		foreach (var line in F.Lines) {
+			var redCount = 0;
+			var greenCount = 0;
+			var blueCount = 0;
+			
+			var suffix = line.Substring(line.IndexOf(":", StringComparison.Ordinal)+2);
+			Console.WriteLine(suffix);
+			string[] rounds = suffix.Split(";", StringSplitOptions.TrimEntries);
+			foreach (var round in rounds) {
+				string[] colors = round.Split(",", StringSplitOptions.TrimEntries);
+				foreach (var color in colors) {
+					string[] d = color.Split(" ");
+					if (d[1].Equals("red")) redCount += int.Parse(d[0]);
+					if (d[1].Equals("green")) greenCount += int.Parse(d[0]);
+					if (d[1].Equals("blue")) blueCount += int.Parse(d[0]);
+				}
+			}
+			Console.WriteLine($"Red: {redCount} Green: {greenCount} Blue: {blueCount}");
+		}
 	}
 }
