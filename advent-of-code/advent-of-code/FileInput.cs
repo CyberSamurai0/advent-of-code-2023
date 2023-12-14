3d4f1c29f31d4dc808e22e69a41c721bb6e05669
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2023-12-14 15:39:39 -0500

    Add build

--- advent-of-code/advent-of-code/FileInput.cs
+++ advent-of-code/advent-of-code/FileInput.cs
@@ -2,16 +2,20 @@ using System.IO;
 
 namespace advent_of_code;
 
-public class FileInput
-{
-    public string Path { get; set; }
+public class FileInput {
+    public string Path { get; }
+    public string Text { get; }
+    public String[] Lines { get; }
 
-    public FileInput(string path)
-    {
+    public FileInput(string path) {
         this.Path = path;
-        if (File.Exists(this.Path))
-        {
+        File.OpenRead(this.Path);
+        if (File.Exists(this.Path)) {
             
+        } else {
+            Console.WriteLine($"File {this.Path} not found");
         }
+        this.Text = File.Exists(this.Path) ? File.ReadAllText(this.Path) : "";
+        this.Lines = File.Exists(this.Path) ? File.ReadAllLines(this.Path) : Array.Empty<string>();
     }
 }
