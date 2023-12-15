937732f018097e385df425b3c86250d37ee7d6da
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2023-12-15 01:11:58 -0500

    Add Day 3

---
+++ advent-of-code/advent-of-code/three/GearRatios.cs
@@ -0,0 +1,14 @@
+namespace advent_of_code.three; 
+
+public class GearRatios {
+	private static readonly FileInput F = new ("./three/input.txt");
+
+	public static void Run() {
+		Console.WriteLine("Advent of Code 2023 Day 3: Gear Ratios");
+		Console.WriteLine($"{F.Lines.Length} loaded");
+
+		foreach (var line in F.Lines) {
+			
+		}
+	}
+}
