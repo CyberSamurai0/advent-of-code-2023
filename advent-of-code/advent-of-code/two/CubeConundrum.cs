5807c52e9d3d905b81f9177fde1e96a57b7e76de
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2023-12-15 01:01:59 -0500

    Finish Day 2 Part 2

--- advent-of-code/advent-of-code/two/CubeConundrum.cs
+++ advent-of-code/advent-of-code/two/CubeConundrum.cs
@@ -2,28 +2,52 @@ namespace advent_of_code.two;
 
 public static class CubeConundrum {
 	private static readonly FileInput F = new ("./two/input.txt");
+	private const int MaxRed = 12;
+	private const int MaxGreen = 13;
+	private const int MaxBlue = 14;
 
 	public static void Run() {
 		Console.WriteLine("Advent of Code 2023 Day 2: Cube Conundrum");
 		Console.WriteLine($"{F.Lines.Length} loaded");
-		foreach (var line in F.Lines) {
-			var redCount = 0;
-			var greenCount = 0;
-			var blueCount = 0;
-			
-			var suffix = line.Substring(line.IndexOf(":", StringComparison.Ordinal)+2);
-			Console.WriteLine(suffix);
+		var sum = 0;
+		var powerSum = 0;
+		for (var i = 0; i < F.Lines.Length; i++) {
+			var line = F.Lines[i];
+
+			var suffix = line.Substring(line.IndexOf(":", StringComparison.Ordinal) + 2);
+			// Console.WriteLine(suffix);
+			bool impossible = false;
+			var minRed = 0;
+			var minGreen = 0;
+			var minBlue = 0;
 			string[] rounds = suffix.Split(";", StringSplitOptions.TrimEntries);
 			foreach (var round in rounds) {
 				string[] colors = round.Split(",", StringSplitOptions.TrimEntries);
 				foreach (var color in colors) {
 					string[] d = color.Split(" ");
-					if (d[1].Equals("red")) redCount += int.Parse(d[0]);
-					if (d[1].Equals("green")) greenCount += int.Parse(d[0]);
-					if (d[1].Equals("blue")) blueCount += int.Parse(d[0]);
+					
+					var colorCount = int.Parse(d[0]);
+					
+					// Part 1 Calculation
+					if (d[1].Equals("red") && colorCount > MaxRed ||
+					    d[1].Equals("green") && colorCount > MaxGreen ||
+					    d[1].Equals("blue") && colorCount > MaxBlue) {
+						impossible = true;
+					};
+					
+					// Part 2 Calculation
+					if (d[1].Equals("red")) minRed = int.Max(minRed, colorCount);
+					if (d[1].Equals("green")) minGreen = int.Max(minGreen, colorCount);
+					if (d[1].Equals("blue")) minBlue = int.Max(minBlue, colorCount);
 				}
 			}
-			Console.WriteLine($"Red: {redCount} Green: {greenCount} Blue: {blueCount}");
+			if (!impossible) sum += i + 1;
+			// else Console.WriteLine($"Game {i + 1} Invalid");
+			
+			// Console.WriteLine($"Game {i + 1} Power: {minRed * minGreen * minBlue} ({minRed}x{minGreen}x{minBlue})");
+			powerSum += minRed * minGreen * minBlue;
 		}
+		Console.WriteLine($"Sum (pt. 1): {sum}");
+		Console.WriteLine($"Power Sum (pt. 2): {powerSum}");
 	}
 }
